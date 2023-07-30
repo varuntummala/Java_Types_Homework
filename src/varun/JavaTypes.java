ca6482b50fdd330a11c3523ddcd8f7ea49f31873
Varun Tummala <varunt@gmail.com>
2023-07-30 19:27:42 -0400

    Final Submission

---
+++ src/varun/JavaTypes.java
@@ -0,0 +1,23 @@
+package src.varun;
+
+public class JavaTypes {
+
+    int y;
+    public static void main(String[] args) {
+        int x;
+
+        String @invalidField;
+
+        String Varun Tummala;
+
+
+        System.out.println("Here is the value of an uninitialized local variable: "+x);
+
+        System.out.println("Trying to print Non Static variable: "+y);
+
+        //Trying to assign Float to an int
+        x=20.0;
+
+
+    }
+}
